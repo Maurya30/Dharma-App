5ac05aaa15ada362f818725a02e8caa77d796168
Maurya30 <m25panch@uwaterloo.ca>
2026-03-14 18:00:16 -0400

    Initial Commit

---
+++ Dharma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dharma
+//
+//  Created by Maurya Panchal on 2026-03-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dharma/DharmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DharmaApp.swift
+//  Dharma
+//
+//  Created by Maurya Panchal on 2026-03-14.
+//
+
+import SwiftUI
+
+@main
+struct DharmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
